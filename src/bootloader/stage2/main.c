3af1f93805e7c79db3da90123652d44d2bfa2351
Ace <mojanghouse@gmail.com>
2022-04-19 18:16:15 +0200

    Update main.c

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -6,7 +6,7 @@ void _cdecl cstart_(uint16_t bootDrive) {
 
     printf("Formatted %% %c %s\r\n", 'a', "string");
     printf("Formatted %d %i %x %p %o %hd %hi %hhu\r\n", 1234, -5678, 0xdead, 0xbeef, 012345, (short) 27, (short) -42, (unsigned char) 20, (char) -10);
-    printf("Formatted %ld %lx %lld %llx\r\n", -100000000001, 0xdeadbeef1, 10200300400, 0xbeef)
+    printf("Formatted %ld %lx %lld %llx\r\n", -100000000001, 0xdeadbeef1, 10200300400, 0xbeef);
 
     for(;;);
 }
