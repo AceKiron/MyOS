3e9cd5535dfdfa2400d4d9e5ee6fd843312437cb
AceKiron <acekiron@outlook.com>
2022-04-19 15:29:29 +0200

    Fix x86.asm

--- src/bootloader/stage2/x86.asm
+++ src/bootloader/stage2/x86.asm
@@ -6,7 +6,7 @@ section _TEXT class=CODE
 ; void _cdecl x86_div64_32(uint64_t dividend, uint32_t divisor, uint32_t* quotientOut, uint64_t* remainderOut)
 ;
 global _x86_div64_32; Exports the _x86_div64_32 function
-_x86_div64_32::
+_x86_div64_32:
     ; Make new call frame
     push bp; Save old call frame
     mov bp, sp; Initialize new call frame
@@ -15,7 +15,7 @@ _x86_div64_32::
 
     ; Divide upper 32 bits
     mov eax, [bp + 4]; eax <- upper 32 bits of dividend
-    mov ecx [bp + 12]; ecx <- divisor
+    mov ecx, [bp + 12]; ecx <- divisor
     xor edx, edx
     div ecx; eax - quot, edx - remainder
 
